ece967af9efcc3cc445150559175e5f832d6cac8
brjun <brj1001@163.com>
2023-02-15 17:25:58 +0800

    示例应用支持支付宝支付

--- addons/demo_rgapi/site.php
+++ addons/demo_rgapi/site.php
@@ -44,27 +44,52 @@ class Demo_rgapiModuleSite extends WeModuleSite {
         include $this->template('other');
     }
 
-    public function doWebPay() {
-        $data = pdo_getall('demo_rgapi_paylog', array(), '', '', 'id DESC');
+    public function doWebWechatpay() {
+        global $_W;
+        $data = pdo_getall('demo_rgapi_paylog', array('type' => 1), '', '', 'id DESC');
         foreach ($data as $key => &$value) {
             $value['createtime'] = date('Y-m-d H:i:s', $value['createtime']);
         }
-        include $this->template('pay');
+        include $this->template('wechatpay');
     }
 
-    public function doWebWechatPay() {
+    public function doWebAlipay() {
         global $_W;
+        $data = pdo_getall('demo_rgapi_paylog', array('type' => 2), '', '', 'id DESC');
+        foreach ($data as $key => &$value) {
+            $value['createtime'] = date('Y-m-d H:i:s', $value['createtime']);
+        }
+        include $this->template('alipay');
+    }
+
+    public function doWebPay() {
+        global $_W, $_GPC;
         try {
+            $type = safe_gpc_string($_GPC['type']);
+            if (empty($type) || !in_array($type, array('wechat', 'ali'))) {
+                iajax(-1, '支付类型错误！');
+            }
             load()->library('sdk-module');
             $api = new \W7\Sdk\Module\Api($_W['setting']['server_setting']['app_id'], $_W['setting']['server_setting']['app_secret'], "1");
-            $pay = $api->wechatPay($_W['siteroot'] . 'payment/wechat/notify.php');
-            $out_trade_no = 'demo' . date('YmdHis', time()) . time() . rand(1111, 9999);
-            $data = $pay->payTransactionsNative("测试支付", $out_trade_no, 1, array('attach' => json_encode(array('uniacid' => $_W['uniacid']))))->toArray();
-            if (empty($data['code_url'])) {
-                iajax(-1, '支付失败！');
+            $out_trade_no = $type . date('YmdHis', time()) . time() . rand(11, 99);
+            if ('wechat' == $type) {
+                $pay = $api->wechatPay($_W['siteroot'] . 'payment/wechat/notify.php');
+                $data = $pay->payTransactionsNative("测试支付", $out_trade_no, 1, array('attach' => json_encode(array('uniacid' => $_W['uniacid']))))->toArray();
+                if (empty($data['code_url'])) {
+                    iajax(-1, '支付失败！');
+                }
+                $code = $data['code_url'];
+            } else {
+                $pay = $api->aliPay($_W['siteroot'] . 'payment/alipay/notify.php');
+                $data = $pay->payForPc("测试支付", $out_trade_no, 0.01)->toArray();
+                if (empty($data['data'])) {
+                    iajax(-1, '支付失败！');
+                }
+                $code = $data['data'];
             }
+
             $log = array(
-                'type' => 'wechat',
+                'type' => 'wechat' == $type ? 'wechat' : 'alipay',
                 'uniacid' => $_W['uniacid'],
                 'acid' => $_W['acid'],
                 'openid' => $_W['member']['uid'],
@@ -79,30 +104,40 @@ class Demo_rgapiModuleSite extends WeModuleSite {
             pdo_insert('core_paylog', $log);
             $insert = array(
                 'no' => $out_trade_no,
-                'code' => $data['code_url'],
+                'code' => $code,
                 'status' => 0,
+                'type' => 'wechat' == $type ? 1 : 2,
                 'createtime' => TIMESTAMP,
                 'updatetime' => TIMESTAMP,
                 'uid' => $_W['uid'],
                 'uniacid' => $_W['uniacid'],
             );
             pdo_insert('demo_rgapi_paylog', $insert);
-            iajax(0, array('code' => $data['code_url'], 'no' => $out_trade_no));
+            iajax(0, array('type' => $type, 'code' => $code, 'no' => $out_trade_no));
         } catch (Exception $e) {
             iajax(-1, '支付失败！错误详情: ' . $e->getMessage());
         }
     }
 
-    public function doWebWechatRefund() {
+    public function doWebRefund() {
         global $_W, $_GPC;
         try {
+            $type = safe_gpc_string($_GPC['type']);
+            if (empty($type) || !in_array($type, array('wechat', 'ali'))) {
+                iajax(-1, '退款类型错误！');
+            }
             load()->library('sdk-module');
             $api = new \W7\Sdk\Module\Api($_W['setting']['server_setting']['app_id'], $_W['setting']['server_setting']['app_secret'], "1");
-            $pay = $api->wechatPay($_W['siteroot'] . 'payment/wechat/refund.php');
             $out_trade_no = safe_gpc_string($_GPC['__input']['no']);
-            $data = $pay->refund($out_trade_no, 1, 1, '', $out_trade_no)->toArray();
-            if (!empty($data['status']) && 'SUCCESS' == $data['status']) {
-                iajax(0, '已申请退款!');
+            if ('wechat' == $type) {
+                $pay = $api->wechatPay($_W['siteroot'] . 'payment/wechat/refund.php');
+                $data = $pay->refund($out_trade_no, 1, 1, '', $out_trade_no)->toArray();
+                if (!empty($data['status']) && 'SUCCESS' == $data['status']) {
+                    iajax(0, '已申请退款!');
+                }
+            } else {
+                $pay = $api->aliPay($_W['siteroot'] . 'payment/alipay/refund.php');
+                $data = $pay->refund($out_trade_no, 0.01)->toArray();
             }
             $refund = array(
                 'uniacid' => $_W['uniacid'],
@@ -113,6 +148,11 @@ class Demo_rgapiModuleSite extends WeModuleSite {
                 'reason' => '',
             );
             pdo_insert('core_refundlog', $refund);
+            if ('ali' == $type && !empty($data['alipay_trade_refund_response']) && 'Success' == $data['alipay_trade_refund_response']['msg']) {
+                pdo_update('core_refundlog', array('status' => 1), array('refund_uniontid' => $out_trade_no));
+                pdo_update('demo_rgapi_paylog', array('status' => 2), array('no' => $out_trade_no));
+                iajax(0, '退款成功!', referer());
+            }
             iajax(0, '已发起退款申请，预计1分钟后退款成功！');
         } catch (Exception $e) {
             iajax(-1, '退款失败！错误详情: ' . $e->getMessage());
